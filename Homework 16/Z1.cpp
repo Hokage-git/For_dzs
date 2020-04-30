1fa3da00db3627a1e7df52358ca2fd3eda482b27
Maksim_Gladkov <60353057+Hokage-git@users.noreply.github.com>
2020-04-30 18:46:29 +0500

    Update Z1.cpp

--- Homework 16/Z1.cpp
+++ Homework 16/Z1.cpp
@@ -36,6 +36,30 @@ char* Uppercase(const char* str1){
 		if (str2[i] >= 'a' && str2[i] <= 'z') str2[i] = str2[i] - 32;
 	}
 	return str2;
+	delete[] str2;
+}
+
+char* Lowercase(char* str1) {
+	int len = strlen(str1);
+	char* str2 = new char[len];
+	strcpy(str2, str1);
+	for (int i = 0; i < len; i++) {
+		if (str2[i] >= 'A' && str2[i] <= 'Z') str2[i] = str2[i] + 32;
+	}
+	return str2;
+	delete[] str2;
+}
+
+char* mystrrev(const char* str) {
+	int len = strlen(str);
+	char* str2 = new char[strlen(str)-5];
+	char temp;
+	for (int i = 0,end=len; i < len; i++,end--) {
+		temp = str[i];
+		str2[end] = temp;
+	}
+	return str2;
+	delete[] str2;
 }
 
 int main()
@@ -44,10 +68,14 @@ int main()
 	const char* str1 = { "I want chiken!" };
 	const char* str2 = { "If you want chiken buy and eat it!!!" };
 	int res=mystrcmp(str1, str2); 
+	char* upper = Uppercase(str1);
+	char* str_rev = mystrrev(str1);
 
 	cout << str1 << endl << str2 << endl;
 	cout << "Если 0 = строки равны, 1 = 1 строка больше 2, -1 = 2 строка больше 1:" << res << endl;
 	cout << "Строка в числах=" << StringToNumber(str1)<<endl;
-	cout << "Строка в верхнем регистре=" << Uppercase(str1);
+	cout << "Строка в верхнем регистре=" << upper << endl;
+	cout << "Эта же cтрока в нижнем регистре=" << Lowercase(upper) << endl;
+	cout << "Строка наоборот=" << str_rev;
 }
 
