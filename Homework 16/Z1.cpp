19695bb12890044f2a426a4fbfe8743f97062536
Maksim_Gladkov <60353057+Hokage-git@users.noreply.github.com>
2020-04-30 18:14:49 +0500

    Update Z1.cpp

--- Homework 16/Z1.cpp
+++ Homework 16/Z1.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#pragma warning(disable : 4996)
 
 using namespace std;
 
@@ -16,12 +18,36 @@ int mystrcmp(const char* str1, const char* str2) {
 	}
 }
 
+int StringToNumber(const char* str) {
+	int len = strlen(str);
+	int sum = 0;
+
+	for (int i = 0; i < len; i++) {
+		sum+=(int)str[i];
+	}
+	return sum;
+}
+
+char* Uppercase(const char* str1){
+	int len = strlen(str1);
+	char* str2=new char[len];
+	strcpy(str2, str1);
+	for (int i = 0; i < len;i++) {
+		if (str2[i] >= 'a' && str2[i] <= 'z') str2[i] = str2[i] - 32;
+	}
+	return str2;
+}
+
 int main()
 {
+	setlocale(LC_ALL, "rus");
 	const char* str1 = { "I want chiken!" };
 	const char* str2 = { "If you want chiken buy and eat it!!!" };
-	int res=mystrcmp(str1, str2);
+	int res=mystrcmp(str1, str2); 
 
-	cout << res;
+	cout << str1 << endl << str2 << endl;
+	cout << "Если 0 = строки равны, 1 = 1 строка больше 2, -1 = 2 строка больше 1:" << res << endl;
+	cout << "Строка в числах=" << StringToNumber(str1)<<endl;
+	cout << "Строка в верхнем регистре=" << Uppercase(str1);
 }
 
